2de64ca529bc08938b354787b26f33562bc52a06
ChristianCamiloP <149966264+ChristianCamiloP@users.noreply.github.com>
2025-03-28 16:17:12 +0000

    c2

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDhgSdpsEd91_M4fa0EMBnbDeDUkwH2ojs",
+  authDomain: "db-preguntas.firebaseapp.com",
+  projectId: "db-preguntas",
+  storageBucket: "db-preguntas.firebasestorage.app",
+  messagingSenderId: "1017897171716",
+  appId: "1:1017897171716:web:a02f9095d2db96d85b2e51"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
